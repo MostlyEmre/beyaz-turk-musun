c7368152190df98e17de127f97e8b68094baf7db
MostlyEmre <6238977+MostlyEmre@users.noreply.github.com>
2023-04-14 18:06:22 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
+import burhan from "./assets/altintop.png";
+import data from "./data/sorular";
+
+function App() {
+  const [score, setScore] = useState(0);
+  const [result, setResult] = useState("");
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+    let score = 0;
+    checkboxes.forEach((checkbox) => {
+      score += Number(+checkbox.value);
+    });
+    setScore(score);
+
+    if (score <= 100) {
+      setResult(`${score} puan ile "Z3NC1"sin.`);
+    } else if (score <= 300 && score >= 200) {
+      setResult(`${score} puan ile "Orta Gelir"sin.`);
+    } else if (score <= 1000 && score >= 300) {
+      setResult(`${score} puan ile "Ucundan Elit"sin.`);
+    } else if (score <= 2000 && score >= 1000) {
+      setResult(`${score} puan ile "Entelektuel"sin.`);
+    } else if (score <= 3000 && score >= 2000) {
+      setResult(`${score} puan ile "Beyaz Turk"sun.`);
+    } else if (score > 3000) {
+      setResult(`${score} puan ile "Bembeyaz Turk"sun.`);
+    }
+  };
+
+  return (
+    <Root className="App">
+      <Header>
+        <Altintop image={burhan} />
+        <div>
+          <p>Ne kadar "Beyaz Turk"sun?</p>
+          <p>Sorulari cevapla, ogren.</p>
+        </div>
+      </Header>
+      <Form>
+        <Checkboxes>
+          {data.map((item, index) => {
+            return (
+              <QuestionWrapper key={index}>
+                <input type="checkbox" id={item.soru} name={item.soru} value={item.puan} />
+                <label htmlFor={item.soru}>{item.soru}</label>
+              </QuestionWrapper>
+            );
+          })}
+        </Checkboxes>
+        <ButtonWrapper>
+          <button type="submit" onClick={(e) => handleClick(e)}>
+            Hesapla
+          </button>
+        </ButtonWrapper>
+
+        <Result>{result}</Result>
+      </Form>
+      <br />
+      <hr />
+
+      <footer>
+        orjinal gorsel:{" "}
+        <a href="/bc6a4cnf.jpg" target="_blank">
+          nekadarbeyazyakalisin.jpg
+        </a>
+        <br />
+        siteyi yapan:{" "}
+        <a href="https://emre.ca" target="_blank">
+          emre
+        </a>
+        <br />
+        eksisosluk linki:{" "}
+        <a href="https://eksisozluk2023.com/ne-kadar-beyaz-turksun-testi--7637464" target="_blank">
+          baslikk
+        </a>
+      </footer>
+    </Root>
+  );
+}
+
+export default App;
+
+const Root = styled.div`
+  padding-top: 20px;
+  padding-bottom: 100px;
+`;
+
+const Header = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  gap: 0;
+  align-items: center;
+  margin-bottom: 10px;
+  background-color: lightgray;
+  padding: 1rem;
+  border-radius: 10px;
+  h1 {
+    margin: 0;
+    margin-bottom: 20px;
+    font-size: 40px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    margin-left: 10px;
+    p {
+      margin: 0;
+    }
+  }
+`;
+
+interface AltintopProps {
+  image: string;
+}
+
+const Altintop = styled.div<AltintopProps>`
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const QuestionWrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 5px;
+  text-align: left;
+
+  label {
+    display: inline-block;
+    cursor: pointer;
+    text-align: left;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  button {
+    margin-top: 1rem;
+    border: 3px solid lightgreen;
+    transition: all 0.5s ease;
+
+    &:hover {
+      border: 3px solid lightblue;
+    }
+  }
+`;
+
+const Checkboxes = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const ButtonWrapper = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
+
+const Result = styled.div``;
---
+++ src/data/sorular.ts
@@ -0,0 +1,90 @@
+export default [
+  {
+    soru: "Ebeveynlerimin ikisi de universite mezunu.",
+    puan: 50,
+  },
+  {
+    soru: "Ayda bir disarida yemek yeriz. (Kebabci haric.)",
+    puan: 50,
+  },
+  {
+    soru: '"Aile dostlarimiz"la disarida kahvalti ederiz.',
+    puan: 100,
+  },
+  {
+    soru: "Hic Kur'an kursuna gitmedim.",
+    puan: 100,
+  },
+  {
+    soru: "Annem turbanli degil.",
+    puan: 50,
+  },
+  {
+    soru: "Dedemler sehirde yasiyor.",
+    puan: 50,
+  },
+  {
+    soru: "Hic devlet okuluna gitmedim.",
+    puan: 100,
+  },
+  {
+    soru: "Pasaportumda gecerli Schengen vizesi var.",
+    puan: 200,
+  },
+  {
+    soru: "Pasaportumda gecerli Amerika vizesi var.",
+    puan: 300,
+  },
+  {
+    soru: "Ebeveynlerim yabanci dil konusabiliyor.",
+    puan: 200,
+  },
+  {
+    soru: "Yasadigimiz sehrin luks bolgesinde kendimize ait evimiz var.",
+    puan: 300,
+  },
+  {
+    soru: "Tatil sehirlerinden birinde yazligimiz var.",
+    puan: 400,
+  },
+  {
+    soru: "Ninem turbanli degil",
+    puan: 200,
+  },
+  {
+    soru: "Evimizde klasik Turk yemekleri disinda degisik kulturlerin yemekleri yapilir.",
+    puan: 50,
+  },
+  {
+    soru: "Dedem veya ninem universite mezunu.",
+    puan: 300,
+  },
+  {
+    soru: "Firsatimiz oldugunda tatilleri yurtdisinda geciririz.",
+    puan: 200,
+  },
+  {
+    soru: "Dini bayramlari kutlamiyoruz.",
+    puan: 100,
+  },
+  {
+    soru: "Evde yemeklerde alkol icilir.",
+    puan: 400,
+  },
+  {
+    soru: "Evimize yillik 50 bin dolar giriyor.",
+    puan: 300,
+  },
+  {
+    soru: "Ebeveynlerimin spor ve sanat gibi hobileri var.",
+    puan: 100,
+  },
+  {
+    soru: "Balkan gocmeniyiz.",
+    puan: 100,
+  },
+  {
+    soru: "Yasadigimiz sehirde soyadimiz meshurdur.",
+    puan: 300,
+  },
+];
